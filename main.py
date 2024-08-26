791ac15ba107e06a87afc5c8e20d4ac9240041c3
xth3usx <matheus.hso89@gmail.com>
2024-08-26 14:51:57 -0300

    26-08_77 last

---
+++ main.py
@@ -0,0 +1,21 @@
+############################################## Parâmetros de Configuração ##############################################
+
+# Acesso Inicial
+ROUTER_ADMIN_URL = '192.168.0.1'
+ROUTER_ADMIN_PASSWORD = '123456'
+
+# Multi-SSID
+SSID_MANAGEMENT_PASSWORD = '123456'
+
+# Configurações Wireless / Nome de Rede (SSID)
+SSID_NAME_2G = 'eduroam'
+SSID_NAME_5G = 'eduroam'
+
+# Configuração Ethernet
+SUBNET_MASK = '255.255.254.0'
+DEFAULT_GATEWAY = '172.24.108.1'
+PRIMARY_DNS = '200.20.0.18'
+SECONDARY_DNS = '200.20.10.17'
+
+# Nome do Grupo de Isolamento
+ISOLATION_GROUP_NAME = 'desabilitados'
