4ab1132b1f6e3e01ea16226e540a31619952e8ce
xth3usx <matheus.hso89@gmail.com>
2025-12-30 02:11:18 -0300

    Update config.py

--- config.py
+++ config.py
@@ -2,20 +2,20 @@
 
 # Acesso Inicial
 URL_ADMIN_ROUTER = '192.168.0.1'
-SENHA_ADMIN_ROUTER = '12345678'
+SENHA_ADMIN_ROUTER = 'XXXXXXXX'
 
 # Multi-SSID
-SENHA_GESTAO_SSID = '12345678' # Mínimo de 8 caracteres (P/ criptografia AES)
+SENHA_GESTAO_SSID = 'XXXXXXXX' # Mínimo de 8 caracteres (P/ criptografia AES)
 
 # Configurações Wireless / Nome de Rede (SSID)
-NOME_SSID_2G = 'eduroam'
-NOME_SSID_5G = 'eduroam'
+NOME_SSID_2G = ''
+NOME_SSID_5G = ''
 
 # Configuração Ethernet
 MASCARA_SUBREDE = '255.255.254.0'
 GATEWAY_PADRAO = '192.168.0.2'
-DNS_PRIMARIO = '10.10.0.1'
-DNS_SECUNDARIO = '10.10.0.2'
+DNS_PRIMARIO = '8.8.8.8'
+DNS_SECUNDARIO = '8.8.4.4'
 
 # Nome do Grupo de Isolamento
 NOME_GRUPO_ISOLAMENTO = 'desabilitados'
